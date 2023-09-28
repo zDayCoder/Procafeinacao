fe23b051c1b85501b887ca056079ceece1130a5a
Rodrigo <rs.strat@gmail.com>
2023-09-28 16:19:00 -0300

    Nav bar

--- header.php
+++ header.php
@@ -1,3 +1,42 @@
-header aqui<br>
-Botão de login::: <a href="./acesso/login">Entrar</a>
-<br><br><br>
+<style>
+body {margin:0;}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: relative;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+</style>
+
+<ul>
+<li><a href="./Procafeinacao/index.php">Home</a></li>
+  <li><a href="./acesso/register">Cadastro</a></li>
+  <li><a href="./acesso/login">Login</a></li>
+  <li style="float:right"><a class="active" href="#about">About</a></li>
+</ul>
+<br><br>
--- index.php
+++ index.php
@@ -9,13 +9,14 @@
     <link href="./pages/css/style.css" rel="stylesheet" type="text/css" />
 
 </head>
-<body>
+<body style="background: url(./pages/img/cardapio.jpg) no-repeat center fixed;
+background-size: cover; " >
     <?php require_once('./header.php');
     //require_once('./initialPage/sites/menu-itens.php');
-    //for($i = 0; $i < 1; $i++){echo "comi seu cu<br>";}?>
+    //for($i = 0; $i < 1; $i++){echo "<br>";}?>
     <!--form method="post">
     <input type="submit" value="enviar"/>
-    </form--> 
+    </form--> <br> <br><br><br>
     <a href="https://preview.themeforest.net/item/koffe-cafe-coffee-shop-template-kit/full_screen_preview/26300402" target="_blank">Site referencia</a>
 
 </body>
